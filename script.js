64858aed85e3afb7ea4150d91cecc313dd484940
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2022-09-13 11:16:15 +0530

    docs: updated project

---
+++ script.js
@@ -0,0 +1,37 @@
+const initialPrice = document.querySelector("#initial-price");
+const noOfStocks = document.querySelector("#no-of-stocks");
+const finalPrice = document.querySelector("#final-price");
+const checkBtn = document.querySelector("#check-btn");
+const result = document.querySelector("#output");
+
+checkBtn.addEventListener("click", clickHandler);
+
+function calculateProfitLoss(initialPrice, noOfStocks, finalPrice) {
+    if (finalPrice > initialPrice) {
+        var profit = (finalPrice - initialPrice) * noOfStocks;
+        var profitPercent = (profit/initialPrice) * 100;
+        result.innerText = `Congrats you made a profit of ${profit} and the profit percent is ${profitPercent}%.`
+    } else
+    if (initialPrice > finalPrice) {
+        var loss = (initialPrice - finalPrice) * noOfStocks;
+        var lossPercent = (loss/initialPrice) * 100;
+        result.innerText = `Oops! You made a loss of ${loss} and the loss percent is ${lossPercent}%.`
+    } else {
+        result.innerText = "You neither faced loss nor profit."
+    }
+}
+
+function clickHandler() {
+    var iPrice = Number(initialPrice.value);
+    var stocks = Number(noOfStocks.value);
+    var fPrice = Number(finalPrice.value);
+    if (iPrice == "") {
+        result.innerText = "Enter Initial Price."
+    } else if (stocks == "") {
+        result.innerText = "Enter NUmber of Stocks."
+    } else if (fPrice == "") {
+        result.innerText = "Enter Final Price."
+    } else {
+        calculateProfitLoss(iPrice, stocks, fPrice);
+    }
+}
